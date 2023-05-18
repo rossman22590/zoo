0b2544d0c41572db047b4f780ccb9e9c6fd08c82
Ross Cohen <rosscohen1990@gmail.com>
2023-05-18 00:53:42 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "ennwjiitmiqwdrgxkevm.supabase.co",
+        hostname: "https://petzxvglmhghvrwkumri.supabase.co",
       },
     ],
   },
